9e038ea2fa69b319f75efaaa9bb68570e85d12f8
LGM16 <lucasgmigliore@gmail.com>
2026-05-21 00:58:14 -0300

    organizacion

--- Reparaciones/main.cpp
+++ Reparaciones/main.cpp
@@ -3,16 +3,20 @@ Reparación de equipamiento informático
 Una empresa que se dedica a la reparación de equipos de informática lleva el registro 
 de los clientes, los equipos que repara y las reparaciones de equipos que realiza a los 
 clientes. 
+
 Por cada uno de los clientes registra el CUIT, el nombre, el apellido, un número de 
 teléfono, un email, la dirección y el tipo de cliente (1: particular; 2: empresa).  
+
 Para cada equipo se registra el número de equipo, la descripción del equipo, la marca, 
 el tipo de equipo, y la fecha en la que ingresó para su reparación. El número de equipo 
 es un código numérico que identifica de forma única a cada equipo. El tipo de equipo 
 se usa para saber si se trata de una PC de escritorio, una notebook, una impresora, un 
 periférico, etc. 
+
 En cuanto a cada una de las reparaciones, a la empresa le interesa registrar cuál es el 
 equipo que se reparó, a qué cliente lo hizo, cuál fue el día de la entrega y cuál fue el 
 importe de la reparación. 
+
 A partir de la descripción se pide: 
 ●  Definir cuáles son los objetos que se necesitan para registrar la información 
 necesaria. 
@@ -20,16 +24,24 @@ necesaria.
 ●  Definir el tipo de dato que corresponda para representar cada propiedad. 
  
 Luego determinar cuáles son las clases y cuáles son los archivos necesarios para el 
-sistema 
+sistema
 */
 
 
 
 #include <iostream>
+//#include "Cliente.h"
+// #include "Equipo.h"
+// #include "Reparacion.h"
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << "Hello, putaso! c:" << endl;
     return 0;
 }
+
+//Cliente: Puede obviarse la edad, suponiendo que al haber tipo particular o empresar, se puede inferir la edad.
+// Preguntar si el CUIT seria equivalente a un id unico (o un id autoincremental).
+//Tecnico: Se puede obviar, suponiendo que la empresa tiene tecnicos fijos, o no es relevante para el sistema, tambien podria incluirse
+//Los técnicos serán los encargados de realizar las reparaciones. De cada técnico se registrará el legajo, apellido, nombre, teléfono y especialidad.
