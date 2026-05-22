634c57f7eca8ee91c6c75dd57b4e0f739c10a22a
LGM16 <lucasgmigliore@gmail.com>
2026-05-22 00:10:15 -0300

    Update Persona.h

--- Reparaciones/Persona.h
+++ Reparaciones/Persona.h
@@ -1,6 +1,31 @@
 //De aca va a heredar Clientes
 #pragma once
+#include <string>
 class Persona{
 protected:
-private:
+    
+    std::string _nombre;
+    std::string _apellido;
+    std::string _cuit;
+
+public:
+
+    //Constructores
+
+    Persona();
+    Persona(std::string nombre, std::string apellido, std::string cuit);
+    
+    //Setters y Getters
+
+    void setNombre(std::string nombre);
+    void setApellido(std::string apellido);
+    void setCuit(std::string cuit);
+    std::string getNombre(){return _nombre;};
+    std::string getApellido(){return _apellido;};
+    std::string getCuit(){return _cuit;};
+
+    //Metodos
+
+    void cargar();
+    void mostrar();
 };
