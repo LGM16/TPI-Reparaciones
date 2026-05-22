57a8bafc3c87806b86f1d2aa6dce49bbeafd5410
LGM16 <lucasgmigliore@gmail.com>
2026-05-22 00:09:47 -0300

    declaracion clases

--- Reparaciones/Clientes.h
+++ Reparaciones/Clientes.h
@@ -4,7 +4,40 @@ teléfono, un email, la dirección y el tipo de cliente (1: particular; 2: empre
 */
 
 #pragma once
+#include "Persona.h"
 class Cliente: public Persona{
 private:
+
+    int _idCliente;
+    int _tipoCliente; //1: particular; 2: empresa
+    std::string _telefono;
+    std::string _email;
+    std::string _direccion;
+    bool _estado;
+
 public:
+
+    //Constructores
+
+    Cliente();
+
+    //Setters y Getters
+
+    void setIdCliente(int idCliente);
+    void setTipoCliente(int tipoCliente);
+    void setTelefono(std::string telefono);
+    void setEmail(std::string email);
+    void setDireccion(std::string direccion);
+    void setEstado(bool estado);
+    int getIdCliente(){return _idCliente;};
+    int getTipoCliente(){return _tipoCliente;};
+    std::string getTelefono(){return _telefono;};
+    std::string getEmail(){return _email;};
+    std::string getDireccion(){return _direccion;};
+    bool getEstado(){return _estado;};
+
+    //Metodos
+
+    void cargar();
+    void mostrar();
 };
--- Reparaciones/Tecnico.h
+++ Reparaciones/Tecnico.h
@@ -1,6 +1,30 @@
 #pragma once
+#include "Persona.h"
 
 class Tecnico: public Persona{
 private:
+
+    int _idTecnico;
+    std::string _usuario;
+    std::string _contrasenia;
+
 public:
+
+    //Constructores
+
+    Tecnico();
+
+    //Setters y Getters
+
+    void setIdTecnico(int idTecnico);
+    void setUsuario(std::string usuario);
+    void setContrasenia(std::string contrasenia);
+    int getIdTecnico(){return _idTecnico;};
+    std::string getUsuario(){return _usuario;};
+    std::string getContrasenia(){return _contrasenia;};
+
+    //Metodos
+
+    void cargar();
+    void mostrar();
 };
