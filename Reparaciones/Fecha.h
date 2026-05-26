809e89d11ad07727e1d047077b7d8de61437eb25
LGM16 <lucasgmigliore@gmail.com>
2026-05-26 02:05:39 -0300

    Update Fecha.h

--- Reparaciones/Fecha.h
+++ Reparaciones/Fecha.h
@@ -2,13 +2,30 @@
 
 class Fecha{
 private:
+    
+    int _dia;
+    int _mes;
+    int _anio;
+
 public:
 
     //Constructores
+    
+    Fecha();
 
     //Setters
 
+    void setDia(int dia);
+    void setMes(int mes);
+    void setAnio(int anio);
+
     //Getters
 
+    int getDia();
+    int getMes();
+    int getAnio();
+
     //Metodos
+
+    bool validarFecha(int dia, int mes, int anio);
 };
