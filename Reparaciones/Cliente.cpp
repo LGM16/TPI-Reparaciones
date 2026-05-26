e55827b2711f46dd9dcd4dd9c7bf9251d9fee0e8
LGM16 <lucasgmigliore@gmail.com>
2026-05-25 22:05:42 -0300

    update

--- Reparaciones/Cliente.cpp
+++ Reparaciones/Cliente.cpp
@@ -25,31 +25,44 @@ void Cliente::setIdCliente(int idCliente){
 }
 
 void Cliente::setTipoCliente(int tipoCliente){
-    if(validarTipoCliente(tipoCliente)){
-        _tipoCliente = tipoCliente;
-    }
-    else{
-        _tipoCliente = 1;
-    }
-    /*
+    
     if(tipoCliente == 1 || tipoCliente == 2){
         _tipoCliente = tipoCliente;
     }
     else{
         _tipoCliente = 1;
     }
+
+    /*
+    if(validarTipoCliente(tipoCliente)){
+        _tipoCliente = tipoCliente;
+    }
+    else{
+        _tipoCliente = 1;
+    }
     */
+    
 }
 
-void Cliente::setTelefono(std::string telefono){
-    _telefono = telefono;
+void Cliente::setTelefono(string telefono){
+    if(telefono.length() >= 8 && telefono.length() <= 15){ //idem .size()
+        _telefono = telefono;
+    }
+    else{
+        _telefono = "";
+    }
 }
 
-void Cliente::setEmail(std::string email){
-    _email = email;
+void Cliente::setEmail(string email){
+    if(email.find('@') != string::npos){ // busca el caracter '@' en la cadena email, si lo encuentra devuelve la posición, sino devuelve string::npos
+        _email = email;
+    }
+    else{
+        _email = "";
+    }
 }
 
-void Cliente::setDireccion(std::string direccion){
+void Cliente::setDireccion(string direccion){
     _direccion = direccion;
 }
 
@@ -62,19 +75,45 @@ void Cliente::setEstado(bool estado){
 void Cliente::cargar(){
 
     int idCliente, tipoCliente;
+    string telefono, email, direccion;
 
     Persona::cargar();
+    // aca puedo hacer una metodo para incrementar el idCliente automaticamente verificando el ultimo cargado
+    //implementar do while en cada carga para validar cada dato ingresado?
     cout << "Ingrese el ID del Cliente: " << endl;
     cin >> idCliente;
     setIdCliente(idCliente);
     cout << "Ingrese el tipo de Cliente (1: Particular, 2: Empresa): " << endl;
     cin >> tipoCliente;
     setTipoCliente(tipoCliente);
+    cout << "Ingrese el telefono del Cliente: " << endl;
+    cin >> telefono;
+    setTelefono(telefono);
+    cout << "Ingrese el email del Cliente: " << endl;
+    cin >> email;
+    setEmail(email);
+    cout << "Ingrese la direccion del Cliente: " << endl;
+    cin >> direccion;
+    setDireccion(direccion);
 }
 
 void Cliente::mostrar(){
     Persona::mostrar();
-
+    cout << "ID del Cliente: " << getIdCliente() << endl;
+    
+    if(getTipoCliente() == 1){
+        cout << "Tipo de Cliente: Particular" << endl;
+    }
+    else{
+        if(getTipoCliente () == 2){
+            cout << "Tipo de Cliente: Empresa" << endl;
+        }
+    }
+    }
+    
+    cout << "Telefono de Cliente: " << getTelefono() << endl;
+    cout << "Email de Cliente: " << getEmail() << endl;
+    cout << "Direccion de Cliente: " << getDireccion() << endl;
 }
 
 bool Cliente::validarTipoCliente(int tipoCliente){
