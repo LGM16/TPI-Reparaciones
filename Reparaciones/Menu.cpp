c20a230c8ac349d9861be9d40dc7ef510833b90d
LGM16 <lucasgmigliore@gmail.com>
2026-05-29 01:43:10 -0300

    Create Class Menu

--- Reparaciones/Menu.cpp
+++ Reparaciones/Menu.cpp
@@ -4,6 +4,25 @@ using namespace std;
 
 ///Constructor
 
+Menu::Menu(){
+    _sesionActiva = false;
+}
+
 ///Setters
 
 ///Metodos
+
+Menu::ejecutar(){
+    while(_sesionActiva){
+        mostrarMenuPrincipal();
+    }
+}
+
+Menu::menuPrincipal(){
+    int opcion;
+    do{
+        cout << "Menu Principal" << endl;
+        
+
+    }while(opcion != 0);
+}
--- Reparaciones/Menu.h
+++ Reparaciones/Menu.h
@@ -1,14 +1,36 @@
 #pragma once
+#include "ManagerArchivo.h"
+#include "Tecnico.h"
 
 class Menu{
 private:
+
+    Tecnico _tecnicoLogueado;
+    ManagerArchivo _manager;
+    bool _sesionActiva = true;
+
 public:
 
     //Constructores
 
+    Menu();
+
     //Setters
+
+    bool setSesionActiva(bool sesionActiva);
 
     //Getters
 
+    bool getSesionActiva();
+
     //Metodos
+
+    void ejecutar();
+    void menuLogin();
+    void menuPrincipal();
+    void menuClientes();
+    void menuEquipos();
+    void menuReparaciones();
+    void menuInformes();
+
 };
