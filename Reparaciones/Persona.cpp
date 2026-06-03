a92377a644267723e9e6fd09451bd6fa9eb63c64
LGM16 <lucasgmigliore@gmail.com>
2026-06-03 00:24:53 -0300

    Update Persona.cpp

--- Reparaciones/Persona.cpp
+++ Reparaciones/Persona.cpp
@@ -5,25 +5,42 @@ using namespace std;
 #include "Persona.h"
 
 ///Constructores
+Persona::Persona():_cuit("Sin Asignar"), _nombre("Lucas"), _apellido("Migliore") {
+}
 
-Persona::Persona(){
-    setCuit("");
-    setNombre("");
-    setApellido("");
+Persona::Persona(string cuit, string nombre, string apellido){
+    setCuit(cuit);
+    setNombre(nombre);
+    setApellido(apellido);
 }
 
 ///Setters
 
 void Persona::setCuit(string cuit){
-    _cuit = cuit;
+    if(cuit.length() == 11){
+        _cuit = cuit;
+    }
+    else{
+        _cuit = "00000000000";
+    }
 }
 
 void Persona::setNombre(string nombre){
-    _nombre = nombre;
+    if(nombre.length() >= 3 && nombre.length() <= 50){
+        _nombre = nombre;
+    }
+    else{
+        _nombre = "Pepe";
+    }
 }
 
 void Persona::setApellido(string apellido){
-    _apellido = apellido;
+    if(apellido.length() >= 3 && apellido.length() <= 50){
+        _apellido = apellido;
+    }
+    else{
+        _apellido = "Lopez";
+    }
 }
 
 ///Metodos
