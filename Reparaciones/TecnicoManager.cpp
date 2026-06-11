3c5787fb23610522e93e7c3e2aedde41ebb05870
LGM16 <lucasgmigliore@gmail.com>
2026-06-10 23:51:21 -0300

    Update TecnicoManager.cpp

--- Reparaciones/TecnicoManager.cpp
+++ Reparaciones/TecnicoManager.cpp
@@ -1,1 +1,11 @@
+#include "TecnicoManager.h"
 
+bool TecnicoManager::validarCredenciales(std::string usuario, std::string contrasenia, Tecnico& tecnico){
+    //Validacion provisoria hasta implementar TecnicoArchivo
+    if(usuario == "admin" && contrasenia == "1234"){
+        Persona persona("20123456789", "Juan", "Perez");
+        tecnico = Tecnico(persona, 1, usuario, contrasenia);
+        return true;
+    }
+    return false;
+}
