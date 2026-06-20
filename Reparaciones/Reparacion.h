0a2bdd871d5106516c8264fc41367a8de078af1c
LGM16 <lucasgmigliore@gmail.com>
2026-06-20 17:46:25 -0300

    Update Reparacion.h

--- Reparaciones/Reparacion.h
+++ Reparaciones/Reparacion.h
@@ -6,22 +6,26 @@
 class Reparacion{
 private:
 
+    int _idReparacion;
     Equipo _equipo;
     Cliente _idCliente;
     Fecha _fechaIngreso;
     Fecha _fechaEgreso;
     float _importe;
-    int _estadoRep; // quiza: 1: en proceso; 2: reparado; 3: no reparado
-    bool _estado = true;
+    int _estadoRep; // 1: en proceso; 2: reparado; 3: no reparado
+    bool _estado = false;
 
 public:
 
     ///Constructores
 
     Reparacion();
+    Reparacion(int idReparacion, Equipo equipo, Cliente idCliente, Fecha fechaIngreso,
+              Fecha fechaEgreso, float importe, int estadoRep, bool estado);
 
     ///Setters
 
+    void setIdReparacion(int idReparacion);
     void setEquipo(Equipo equipo);
     void setIdCliente(Cliente idCliente);
     void setFechaIngreso(Fecha fechaIngreso);
@@ -32,13 +36,14 @@ public:
 
     ///Getters
 
-    Equipo getEquipo();
-    Cliente getIdCliente();
-    Fecha getFechaIngreso();
-    Fecha getFechaEgreso();
-    float getImporte();
-    int getEstadoRep();
-    bool getEstado();
+    int getIdReparacion(){return _idReparacion;};
+    Equipo getEquipo(){return _equipo;};
+    Cliente getIdCliente(){return _idCliente;};
+    Fecha getFechaIngreso(){return _fechaIngreso;};
+    Fecha getFechaEgreso(){return _fechaEgreso;};
+    float getImporte(){return _importe;};
+    int getEstadoRep(){return _estadoRep;};
+    bool getEstado(){return _estado;};
 
     ///Metodos
 };
